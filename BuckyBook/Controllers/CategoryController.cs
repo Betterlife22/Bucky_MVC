4c4bea2ff211668a5f6e046baa8311dd87832788
Tunghdse173573 <166358537+Tunghdse173573@users.noreply.github.com>
2024-05-16 10:07:12 +0700

    add table database category và thay đổi bootstrap

--- BuckyBook/Controllers/CategoryController.cs
+++ BuckyBook/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BuckyBook.Data;
+using BuckyBook.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BuckyBook.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _dbContext= db;
+        }
         public IActionResult Index()
         {
-            return View();
+            // vao database lấy list ra gán vào list objCategory để từ đó ngang bằng vs select *
+            List<Category> objCategory= _dbContext.Categories.ToList();
+            return View(objCategory);
         }
     }
 }
